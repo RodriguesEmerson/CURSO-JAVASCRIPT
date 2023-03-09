74a4ac24f961c8e019df6185425ed5f0c84df408
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2023-03-09 16:54:24 -0300

    Update index.js

--- Aula-1/index.js
+++ Aula-1/index.js
@@ -1,5 +1,24 @@
 
-let a = [ 1, 3, 4, 6]
-let b = [...a];
-b[0] = 'Teste';
-console.log(b)
+/**Copiando um array para que cada um seja independete */
+// let a = [ 1, 3, 4, 6]
+// let b = [...a];
+// b[0] = 'Teste';
+// console.log(b)
+ 
+/**Atribuição via Desestruturação Arrays */
+// let arr = ['foi', 'teste', 'ola'];
+// let [a, b, c] = arr;
+// console.log(b)
+
+/**Atribuição via Desestruturação Objects */
+const user = {
+    nome: 'Emerson',
+    idade: 24,
+    job: 'Developer',
+    edereco: {
+        rua: 'Exemple Street',
+        numero: 321
+    }
+}
+const {nome = 'Vazio', idade, job: servico, edereco: {rua, numero}} = user
+console.log(nome, idade, servico, rua, numero)
