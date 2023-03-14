2dbc5bd0d11f89638703b229983d4edaf0baf488
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2023-03-14 11:25:30 -0300

    Create script.js

---
+++ Aula-10/script.js
@@ -0,0 +1,11 @@
+let user = {
+    nome: 'Emerson',
+    sobrenome: 'Rodrigues',
+    ultimoNome: 'Cordeiro'
+}
+
+const lastName = 'ultimoNome'
+
+console.log(user.nome);
+console.log(user['sobrenome']);
+console.log(user[lastName]);
